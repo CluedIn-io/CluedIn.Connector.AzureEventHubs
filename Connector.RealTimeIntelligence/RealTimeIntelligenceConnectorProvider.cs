a390c5971d70b0a9ae89b2b39c8c4c323d8690ad
Andrew Hardy <aha@cluedin.com>
2024-09-18 17:19:09 +1000

    ran resharper code cleanup for new projects

--- Connector.RealTimeIntelligence/RealTimeIntelligenceConnectorProvider.cs
+++ Connector.RealTimeIntelligence/RealTimeIntelligenceConnectorProvider.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using CluedIn.Connector.AzureEventHub;
 using CluedIn.Core;
 using CluedIn.Core.Crawling;
@@ -9,163 +6,188 @@ using CluedIn.Core.Providers;
 using CluedIn.Core.Webhooks;
 using CluedIn.Providers.Models;
 using Newtonsoft.Json;
+using ExecutionContext = CluedIn.Core.ExecutionContext;
 
-namespace CluedIn.Connector.RealTimeIntelligence
+namespace CluedIn.Connector.RealTimeIntelligence;
+
+public class RealTimeIntelligenceConnectorProvider : ProviderBase, IExtendedProviderMetadata
 {
-    public class RealTimeIntelligenceConnectorProvider : ProviderBase, IExtendedProviderMetadata
+    /**********************************************************************************************************
+     * CONSTRUCTORS
+     **********************************************************************************************************/
+
+    public RealTimeIntelligenceConnectorProvider([NotNull] ApplicationContext appContext)
+        : base(appContext, RealTimeIntelligenceConstants.CreateProviderMetadata())
     {
-        /**********************************************************************************************************
-         * CONSTRUCTORS
-         **********************************************************************************************************/
+    }
 
-        public RealTimeIntelligenceConnectorProvider([NotNull] ApplicationContext appContext)
-            : base(appContext, RealTimeIntelligenceConstants.CreateProviderMetadata())
+    public string ServiceType { get; } = JsonConvert.SerializeObject(RealTimeIntelligenceConstants.ServiceType);
+    public string Aliases { get; } = JsonConvert.SerializeObject(RealTimeIntelligenceConstants.Aliases);
+
+    public string Details { get; set; } = RealTimeIntelligenceConstants.Details;
+    public string Category { get; set; } = RealTimeIntelligenceConstants.Category;
+
+    public string Icon => RealTimeIntelligenceConstants.IconResourceName;
+    public string Domain { get; } = RealTimeIntelligenceConstants.Uri;
+    public string About { get; } = RealTimeIntelligenceConstants.ConnectorDescription;
+    public AuthMethods AuthMethods => RealTimeIntelligenceConstants.AuthMethods;
+    public IEnumerable<Control> Properties { get; } = RealTimeIntelligenceConstants.Properties;
+
+    public Guide Guide { get; set; } = new() { Instructions = RealTimeIntelligenceConstants.Instructions, Value = new List<string> { RealTimeIntelligenceConstants.ConnectorDescription }, Details = RealTimeIntelligenceConstants.Details };
+
+    public new IntegrationType Type { get; set; } = RealTimeIntelligenceConstants.Type;
+
+    /**********************************************************************************************************
+     * METHODS
+     **********************************************************************************************************/
+
+    public override async Task<CrawlJobData> GetCrawlJobData(
+        ProviderUpdateContext context,
+        IDictionary<string, object> configuration,
+        Guid organizationId,
+        Guid userId,
+        Guid providerDefinitionId)
+    {
+        if (configuration == null)
         {
-           
+            throw new ArgumentNullException(nameof(configuration));
         }
 
-        /**********************************************************************************************************
-         * METHODS
-         **********************************************************************************************************/
+        var result = new AzureEventHubConnectorJobData(configuration);
 
-        public override async Task<CrawlJobData> GetCrawlJobData(
-            ProviderUpdateContext context,
-            IDictionary<string, object> configuration,
-            Guid organizationId,
-            Guid userId,
-            Guid providerDefinitionId)
+        return await Task.FromResult(result);
+    }
+
+    public override Task<bool> TestAuthentication(
+        ProviderUpdateContext context,
+        IDictionary<string, object> configuration,
+        Guid organizationId,
+        Guid userId,
+        Guid providerDefinitionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<ExpectedStatistics> FetchUnSyncedEntityStatistics(ExecutionContext context, IDictionary<string, object> configuration, Guid organizationId, Guid userId, Guid providerDefinitionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<IDictionary<string, object>> GetHelperConfiguration(
+        ProviderUpdateContext context,
+        [NotNull] CrawlJobData jobData,
+        Guid organizationId,
+        Guid userId,
+        Guid providerDefinitionId)
+    {
+        if (jobData == null)
         {
-            if (configuration == null)
-                throw new ArgumentNullException(nameof(configuration));
-
-            var result = new AzureEventHubConnectorJobData(configuration);
-
-            return await Task.FromResult(result);
+            throw new ArgumentNullException(nameof(jobData));
         }
 
-        public override Task<bool> TestAuthentication(
-            ProviderUpdateContext context,
-            IDictionary<string, object> configuration,
-            Guid organizationId,
-            Guid userId,
-            Guid providerDefinitionId)
+        if (jobData is AzureEventHubConnectorJobData result)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(result.ToDictionary());
         }
 
-        public override Task<ExpectedStatistics> FetchUnSyncedEntityStatistics(Core.ExecutionContext context, IDictionary<string, object> configuration, Guid organizationId, Guid userId, Guid providerDefinitionId)
+        throw new InvalidOperationException($"Unexpected data type for AzureEventConnectorJobData, {jobData.GetType()}");
+    }
+
+    public override Task<IDictionary<string, object>> GetHelperConfiguration(
+        ProviderUpdateContext context,
+        CrawlJobData jobData,
+        Guid organizationId,
+        Guid userId,
+        Guid providerDefinitionId,
+        string folderId)
+    {
+        return GetHelperConfiguration(context, jobData, organizationId, userId, providerDefinitionId);
+    }
+
+    public override Task<AccountInformation> GetAccountInformation(ExecutionContext context, [NotNull] CrawlJobData jobData, Guid organizationId, Guid userId, Guid providerDefinitionId)
+    {
+        if (jobData == null)
         {
-            throw new NotImplementedException();
+            throw new ArgumentNullException(nameof(jobData));
         }
 
-        public override Task<IDictionary<string, object>> GetHelperConfiguration(
-            ProviderUpdateContext context,
-            [NotNull] CrawlJobData jobData,
-            Guid organizationId,
-            Guid userId,
-            Guid providerDefinitionId)
+        if (!(jobData is AzureEventHubConnectorJobData result))
         {
-            if (jobData == null)
-                throw new ArgumentNullException(nameof(jobData));
-
-            if (jobData is AzureEventHubConnectorJobData result)
-            {
-                return Task.FromResult(result.ToDictionary());
-            }
-
-            throw new InvalidOperationException($"Unexpected data type for AzureEventConnectorJobData, {jobData.GetType()}");
+            throw new ArgumentException(
+                "Wrong CrawlJobData type", nameof(jobData));
         }
 
-        public override Task<IDictionary<string, object>> GetHelperConfiguration(
-            ProviderUpdateContext context,
-            CrawlJobData jobData,
-            Guid organizationId,
-            Guid userId,
-            Guid providerDefinitionId,
-            string folderId)
+        var accountId = $"{result.Name}.{result.ConnectionString}";
+
+        return Task.FromResult(new AccountInformation(accountId, $"{accountId}"));
+    }
+
+    public override string Schedule(DateTimeOffset relativeDateTime, bool webHooksEnabled)
+    {
+        return $"{relativeDateTime.Minute} 0/23 * * *";
+    }
+
+    public override Task<IEnumerable<WebHookSignature>> CreateWebHook(ExecutionContext context, [NotNull] CrawlJobData jobData, [NotNull] IWebhookDefinition webhookDefinition, [NotNull] IDictionary<string, object> config)
+    {
+        if (jobData == null)
         {
-            return GetHelperConfiguration(context, jobData, organizationId, userId, providerDefinitionId);
+            throw new ArgumentNullException(nameof(jobData));
         }
 
-        public override Task<AccountInformation> GetAccountInformation(Core.ExecutionContext context, [NotNull] CrawlJobData jobData, Guid organizationId, Guid userId, Guid providerDefinitionId)
+        if (webhookDefinition == null)
         {
-            if (jobData == null)
-            {
-                throw new ArgumentNullException(nameof(jobData));
-            }
-
-            if (!(jobData is AzureEventHubConnectorJobData result))
-            {
-                throw new ArgumentException(
-                    "Wrong CrawlJobData type", nameof(jobData));
-            }
-
-            var accountId = $"{result.Name}.{result.ConnectionString}";
-
-            return Task.FromResult(new AccountInformation(accountId, $"{accountId}"));
+            throw new ArgumentNullException(nameof(webhookDefinition));
         }
 
-        public override string Schedule(DateTimeOffset relativeDateTime, bool webHooksEnabled)
+        if (config == null)
         {
-            return $"{relativeDateTime.Minute} 0/23 * * *";
+            throw new ArgumentNullException(nameof(config));
         }
 
-        public override Task<IEnumerable<WebHookSignature>> CreateWebHook(Core.ExecutionContext context, [NotNull] CrawlJobData jobData, [NotNull] IWebhookDefinition webhookDefinition, [NotNull] IDictionary<string, object> config)
-        {
-            if (jobData == null) throw new ArgumentNullException(nameof(jobData));
-            if (webhookDefinition == null) throw new ArgumentNullException(nameof(webhookDefinition));
-            if (config == null) throw new ArgumentNullException(nameof(config));
+        throw new NotImplementedException();
+    }
 
-            throw new NotImplementedException();
+    public override Task<IEnumerable<WebhookDefinition>> GetWebHooks(ExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task DeleteWebHook(ExecutionContext context, [NotNull] CrawlJobData jobData, [NotNull] IWebhookDefinition webhookDefinition)
+    {
+        if (jobData == null)
+        {
+            throw new ArgumentNullException(nameof(jobData));
         }
 
-        public override Task<IEnumerable<WebhookDefinition>> GetWebHooks(Core.ExecutionContext context)
+        if (webhookDefinition == null)
         {
-            throw new NotImplementedException();
+            throw new ArgumentNullException(nameof(webhookDefinition));
         }
 
-        public override Task DeleteWebHook(Core.ExecutionContext context, [NotNull] CrawlJobData jobData, [NotNull] IWebhookDefinition webhookDefinition)
-        {
-            if (jobData == null) throw new ArgumentNullException(nameof(jobData));
-            if (webhookDefinition == null) throw new ArgumentNullException(nameof(webhookDefinition));
+        throw new NotImplementedException();
+    }
 
-            throw new NotImplementedException();
+    public override IEnumerable<string> WebhookManagementEndpoints([NotNull] IEnumerable<string> ids)
+    {
+        if (ids == null)
+        {
+            throw new ArgumentNullException(nameof(ids));
         }
 
-        public override IEnumerable<string> WebhookManagementEndpoints([NotNull] IEnumerable<string> ids)
+        // TODO should ids also be checked for being empty ?
+
+        throw new NotImplementedException();
+    }
+
+    public override Task<CrawlLimit> GetRemainingApiAllowance(ExecutionContext context, [NotNull] CrawlJobData jobData, Guid organizationId, Guid userId, Guid providerDefinitionId)
+    {
+        if (jobData == null)
         {
-            if (ids == null) throw new ArgumentNullException(nameof(ids));
-
-            // TODO should ids also be checked for being empty ?
-
-            throw new NotImplementedException();
+            throw new ArgumentNullException(nameof(jobData));
         }
 
-        public override Task<CrawlLimit> GetRemainingApiAllowance(Core.ExecutionContext context, [NotNull] CrawlJobData jobData, Guid organizationId, Guid userId, Guid providerDefinitionId)
-        {
-            if (jobData == null) throw new ArgumentNullException(nameof(jobData));
-
-            //TODO what the hell is this?
-            //There is no limit set, so you can pull as often and as much as you want.
-            return Task.FromResult(new CrawlLimit(-1, TimeSpan.Zero));
-        }
-
-        public string Icon => RealTimeIntelligenceConstants.IconResourceName;
-        public string Domain { get; } = RealTimeIntelligenceConstants.Uri;
-        public string About { get; } = RealTimeIntelligenceConstants.ConnectorDescription;
-        public AuthMethods AuthMethods => RealTimeIntelligenceConstants.AuthMethods;
-        public IEnumerable<Control> Properties { get; } = RealTimeIntelligenceConstants.Properties;
-        public string ServiceType { get; } = JsonConvert.SerializeObject(RealTimeIntelligenceConstants.ServiceType);
-        public string Aliases { get; } = JsonConvert.SerializeObject(RealTimeIntelligenceConstants.Aliases);
-        public Guide Guide { get; set; } = new Guide {
-            Instructions = RealTimeIntelligenceConstants.Instructions,
-            Value = new List<string> { RealTimeIntelligenceConstants.ConnectorDescription },
-            Details = RealTimeIntelligenceConstants.Details
-
-        };
-
-        public string Details { get; set; } = RealTimeIntelligenceConstants.Details;
-        public string Category { get; set; } = RealTimeIntelligenceConstants.Category;
-        public new IntegrationType Type { get; set; } = RealTimeIntelligenceConstants.Type;
+        //TODO what the hell is this?
+        //There is no limit set, so you can pull as often and as much as you want.
+        return Task.FromResult(new CrawlLimit(-1, TimeSpan.Zero));
     }
 }
--- src/Connector.DataActivator/DataActivatorConnectorComponent.cs
+++ src/Connector.DataActivator/DataActivatorConnectorComponent.cs
@@ -1,66 +1,59 @@
-using System;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Castle.MicroKernel.Registration;
 using CluedIn.Connector.AzureEventHub;
-using CluedIn.Connector.AzureEventHub.Connector;
 using CluedIn.Core;
-using CluedIn.Core.Accounts;
 using CluedIn.Core.Providers;
 using CluedIn.Core.Server;
-using CluedIn.Core.Streams;
-using CluedIn.Core.Streams.Models;
 using ComponentHost;
 using Microsoft.Extensions.Logging;
 
-namespace CluedIn.Connector.DataActivator
+namespace CluedIn.Connector.DataActivator;
+
+[Component(DataActivatorConstants.ProviderName, "Providers", ComponentType.Service, ServerComponents.ProviderWebApi, Components.Server, Components.DataStores, Isolation = ComponentIsolation.NotIsolated)]
+public sealed class DataActivatorConnectorComponent : ServiceApplicationComponent<IServer>
 {
-    [Component(DataActivatorConstants.ProviderName, "Providers", ComponentType.Service, ServerComponents.ProviderWebApi, Components.Server, Components.DataStores, Isolation = ComponentIsolation.NotIsolated)]
-    public sealed class DataActivatorConnectorComponent : ServiceApplicationComponent<IServer>
+    /**********************************************************************************************************
+     * CONSTRUCTOR
+     **********************************************************************************************************/
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="AzureEventHubConnectorComponent" /> class.
+    /// </summary>
+    /// <param name="componentInfo">The component information.</param>
+    public DataActivatorConnectorComponent(ComponentInfo componentInfo) : base(componentInfo)
     {
-        /**********************************************************************************************************
-         * CONSTRUCTOR
-         **********************************************************************************************************/
+        // Dev. Note: Potential for compiler warning here ... CA2214: Do not call overridable methods in constructors
+        //   this class has been sealed to prevent the CA2214 waring being raised by the compiler
+        Container.Register(Component.For<DataActivatorConnectorComponent>().Instance(this));
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AzureEventHubConnectorComponent" /> class.
-        /// </summary>
-        /// <param name="componentInfo">The component information.</param>
-        public DataActivatorConnectorComponent(ComponentInfo componentInfo) : base(componentInfo)
+        //Container.Register(Component.For<ISqlClient>().ImplementedBy<SqlClient>().OnlyNewServices());
+    }
+
+    /**********************************************************************************************************
+     * METHODS
+     **********************************************************************************************************/
+
+    /// <summary>Starts this instance.</summary>
+    public override void Start()
+    {
+        //Container.Install(new InstallComponents());
+
+        var asm = Assembly.GetExecutingAssembly();
+        Container.Register(Types.FromAssembly(asm).BasedOn<IProvider>().WithServiceFromInterface().If(t => !t.IsAbstract).LifestyleSingleton());
+        // Container.Register(Types.FromAssembly(asm).BasedOn<IEntityActionBuilder>().WithServiceFromInterface().If(t => !t.IsAbstract).LifestyleSingleton());
+
+        Log.LogInformation("[DataActivator] Data Activator Registered");
+        State = ServiceState.Started;
+    }
+
+    /// <summary>Stops this instance.</summary>
+    public override void Stop()
+    {
+        if (State == ServiceState.Stopped)
         {
-            // Dev. Note: Potential for compiler warning here ... CA2214: Do not call overridable methods in constructors
-            //   this class has been sealed to prevent the CA2214 waring being raised by the compiler
-            Container.Register(Component.For<DataActivatorConnectorComponent>().Instance(this));
-
-            //Container.Register(Component.For<ISqlClient>().ImplementedBy<SqlClient>().OnlyNewServices());
+            return;
         }
 
-        /**********************************************************************************************************
-         * METHODS
-         **********************************************************************************************************/
-
-        /// <summary>Starts this instance.</summary>
-        public override void Start()
-        {
-
-            //Container.Install(new InstallComponents());
-
-            var asm = Assembly.GetExecutingAssembly();
-            Container.Register(Types.FromAssembly(asm).BasedOn<IProvider>().WithServiceFromInterface().If(t => !t.IsAbstract).LifestyleSingleton());
-           // Container.Register(Types.FromAssembly(asm).BasedOn<IEntityActionBuilder>().WithServiceFromInterface().If(t => !t.IsAbstract).LifestyleSingleton());
-
-            this.Log.LogInformation("[DataActivator] Data Activator Registered");
-            State = ServiceState.Started;
-        }
-
-        /// <summary>Stops this instance.</summary>
-        public override void Stop()
-        {
-            if (State == ServiceState.Stopped)
-                return;
-
-            State = ServiceState.Stopped;
-        }
+        State = ServiceState.Stopped;
     }
 }
